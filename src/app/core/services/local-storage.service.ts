1160425a74b0b8a83706e3e0f358c4d52ecf2f21
upskill js.basics <upskill.js.basics@gmail.com>
2021-02-03 19:58:51 +0200

    task3-comments

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -6,6 +6,7 @@ export const localStorageKeyToken = new InjectionToken<string>('KeyToken');
 export class LocalStorageService {
   // i'm not sure why do we need useValue in this task, so i'm using it for key
   // (even if it is stupid from practical point of view)
+  // Сегодня я объяснил ситуацию, надеюсь понятна цель задания.
   constructor(
     @Inject(localStorageKeyToken) private key: string) { }
 
